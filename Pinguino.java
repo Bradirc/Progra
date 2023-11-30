84ef6a8c55d3ff30b097c2a5be57390bd36c0076
Bradirc <150397018+Bradirc@users.noreply.github.com>
2023-11-29 22:02:57 -0500

    Add files via upload

---
+++ Pinguino.java
@@ -0,0 +1,19 @@
+
+public class Pinguino extends Zoo {
+    public Pinguino(String nombrePublico, int numeroRegistro) {
+        super(nombrePublico, numeroRegistro);
+    }
+
+    public void nadar() {
+        System.out.println("El pinguino en el zoo esta nadando.");
+    }
+
+    public void pescar() {
+        System.out.println("El pinguino en el zoo esta pescando.");
+    }
+}
+
+
+
+
+   
