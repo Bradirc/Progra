178d1b8836f9dd1262bbbf83a2f72fb18d7078e6
Bradirc <150397018+Bradirc@users.noreply.github.com>
2023-11-29 22:03:51 -0500

    Add files via upload

---
+++ Leon.java
@@ -0,0 +1,15 @@
+
+public class Leon extends Zoo {
+    public Leon(String nombrePublico, int numeroRegistro) {
+        super(nombrePublico, numeroRegistro);
+    }
+
+    public void rugir() {
+        System.out.println("El león en el zoo esta rugiendo.");
+    }
+
+    public void cazar() {
+        System.out.println("El león en el zoo esta cazando.");
+    }
+}
+    
