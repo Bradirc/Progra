94d9b48675ca6b68791d0bdc68155f7f1bb2ead9
Bradirc <150397018+Bradirc@users.noreply.github.com>
2023-12-13 23:35:51 -0500

    Add files via upload

---
+++ Codigoprueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.codigoprueba;
+
+/**
+ *
+ * @author BRADI
+ */
+public class Codigoprueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
